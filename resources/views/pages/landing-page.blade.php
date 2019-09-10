d2dd0b87c8b24e544c15cdf331ac99ca7dbe5b54
Bisyairi <syairidev@gmail.com>
2019-09-10 09:33:48 +0800

    images

--- resources/views/pages/landing-page.blade.php
+++ resources/views/pages/landing-page.blade.php
@@ -5,8 +5,8 @@
 		<div class="slider-area">
 			<div class="bend niceties preview-1">
 				<div id="ensign-nivoslider-3" class="slides">
-					<img src="{{asset('img/slider/s-1-1.jpg')}}" alt="" title="#slider-direction-1"/>
- 					<img src="{{asset('img/slider/s-1-2.jpg')}}" alt="" title="#slider-direction-2"/>
+					<img src="{{asset('img/slider/1.jpg')}}" alt="" title="#slider-direction-1"/>
+ 					<img src="{{asset('img/slider/2.jpg')}}" alt="" title="#slider-direction-2"/>
 				</div>
 				<!-- direction 1 -->
 				<div id="slider-direction-1" class="t-cn slider-direction">
--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -6,7 +6,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h2>SHOP LEFT SIDEBAR</h2>
+                    <h2></h2>
                 </div>
             </div>
         </div>
